7aa980eea7ae8e8d023963a1b0e31f3b4955027d
josoder <j.soderstrand@gmail.com>
2017-10-20 21:25:39 +0200

    added parent pom and switched the modules parent

--- springQuiz/quiz/src/main/kotlin/no/group3/springQuiz/quiz/model/dto/CategoryDto.kt
+++
@@ -1,8 +0,0 @@
-package no.group3.springQuiz.quiz.model.dto
-
-
-
-/**
- * Created by josoder on 18.10.17.
- */
-data class CategoryDto(var s:String = "j")
--- springQuiz/quiz/src/main/kotlin/no/group3/springQuiz/quiz/model/dto/QuizDto.kt
+++
@@ -1,5 +0,0 @@
-package no.group3.springQuiz.quiz.model.dto
-
-/**
- * Created by josoder on 18.10.17.
- */
