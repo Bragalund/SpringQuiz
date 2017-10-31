5ab576e379b47d10a12239557ad62fd67c993261
josoder <j.soderstrand@gmail.com>
2017-10-31 22:18:04 +0100

    added swagger config

--- quiz/src/main/kotlin/no/group3/springQuiz/quiz/QuizApplication.kt
+++ quiz/src/main/kotlin/no/group3/springQuiz/quiz/QuizApplication.kt
@@ -7,6 +7,13 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @SpringBootApplication
@@ -18,6 +25,28 @@ class QuizApplication{
         questionRepository.save(Question(questionText = "who am I?", answers = listOf("?", "?", "js", "?"),
                 correctAnswers = 2))
     }*/
+}
+
+@EnableSwagger2
+@Configuration
+class config{
+    @Bean
+    fun swaggerApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .build()
+    }
+
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+                .title("API for quiz")
+                .description("pg6100 assignment")
+                .version("1.0")
+                .build()
+    }
 }
 
 fun main(args: Array<String>) {
