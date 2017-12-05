c263758addebc024eceb321627b90e2d544ecfee
josoder <j.soderstrand@gmail.com>
2017-12-05 15:05:09 +0100

    user-service is now integrated with zuul and eureka /user/apiV1

--- user/src/test/kotlin/no/group3/user/UserTestBase.kt
+++ user/src/test/kotlin/no/group3/user/UserTestBase.kt
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(classes = arrayOf(UserApplication::class),
+@SpringBootTest("eureka.client.enabled:false" ,classes = arrayOf(UserApplication::class),
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class UserTestBase{
 
