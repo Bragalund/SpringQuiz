64533eeb471b88584508a1ebff82ad7b56f19000
husjoh15 <husjoh15@student.westerdals.no>
2017-12-13 14:28:25 +0100

    fixed typo

--- highscore/src/test/kotlin/no/group3/springQuiz/highscore/api/ScoreTest.kt
+++ highscore/src/test/kotlin/no/group3/springQuiz/highscore/api/ScoreTest.kt
@@ -47,10 +47,9 @@ class ScoreTest : ScoreTestBase(){
         val score1 = addScore(user="Kjell", score=5)
         val score2 = addScore(user="Lars", score=3)
         val score3 = addScore(user="Jonas", score=8)
-        val scores = get(HIGHSCORE_PATH).then().extract().asByteArray()
+        get(HIGHSCORE_PATH).then().body("size()", equalTo(0))
 
-
-
+        
     }
 
     @Test
