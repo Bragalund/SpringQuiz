a9daa56139dfd6049d0cdcbf3f612d51d54f02c8
Bragalund <bakhen15@student.westerdals.no>
2017-12-13 14:14:12 +0100

    Made username unique

--- user/src/main/kotlin/no/group3/SpringQuiz/user/model/entity/User.kt
+++ user/src/main/kotlin/no/group3/SpringQuiz/user/model/entity/User.kt
@@ -1,6 +1,6 @@
 package no.group3.SpringQuiz.user.model.entity
 
-import org.hibernate.validator.constraints.NotEmpty
+import org.hibernate.validator.constraints.*
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 
@@ -11,7 +11,7 @@ class User(
         @get: Id @get: GeneratedValue
         var userId: Long? = null,
 
-        @get: NotEmpty @get: NotNull
+        @get: NotEmpty @get: NotNull @get: Column(unique = true)
         var userName: String? = null,
 
         @get: NotEmpty @get: NotNull
